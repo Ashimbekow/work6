2d7bb0b5ae95f0a865e57eba6c0b0d1ed24284d2
Ashimbekow <Ashimbekoverbolot@gmail.com>
2022-10-23 15:33:59 +0330

    two

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const obj2 = {
+   height:200,
+   width:300,
+   tittle:"My block"
+} 
+
+
+
+function someCalc(a, b) {
+   let ret = a ** b
+   return console.log(ret)
+}
+
+someCalc(200, 2)
+
+
+
+function someCalc(a, b) {
+   let met = a ** b
+   return console.log(met)
+}
+
+someCalc(300, 2)
+
+//Второй вариант.
+// var sum1 = obj2.height**2
+// var sum2 = obj2.width**2
+// console.log(sum1);
+// console.log(sum2);
